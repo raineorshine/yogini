1e58fce1e4efb2dcaa47c5ae8cc79dd929c9857e
metaraine <raineorshine@gmail.com>
2016-01-04 15:05:20 -0700

    Re-add camelize to viewData.

--- app/index.js
+++ app/index.js
@@ -78,6 +78,7 @@ module.exports = generators.Base.extend({
 
       // populate viewData from the prompts and formatted values
       this.viewData = R.merge(props, {
+        camelize: camelize,
         keywordsFormatted: stringifyIndented(parseArray(props.keywords), ' ', 2)
       })
 
