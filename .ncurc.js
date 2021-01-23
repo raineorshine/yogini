d46363ddc3edf07fe4861da1a0243a6c11680315
Raine Revere <raine@cybersemics.org>
2021-01-23 16:01:17 -0700

    Add .ncurc for yeoman-generator dependency notes.

---
+++ .ncurc.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  // 'yeoman-generator'
+  // use custom yeoman-generator to include mem-fs-editor >= 8.0.0 (requires node >= v12)
+  // allows quoted prefixnote expressions
+  // https://github.com/raineorshine/yogini/pull/4
+
+}
